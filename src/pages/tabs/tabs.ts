3bb7692ab1c6a09bfd8eb10de4e86abea8727679
matheuspsantos <matheuspereira8187@gmail.com>
2018-06-27 00:45:14 -0300

    Menu de perfil mudado para o Tabs

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,6 +1,7 @@
-import { CartPage } from './../cart/cart';
-import { MercadosPage } from './../mercados/mercados';
+import { NavController, ModalController} from 'ionic-angular';
 import { PromosPage } from './../promos/promos';
+import { HomePage } from './../home/home';
+import { CartPage } from './../cart/cart';
 import { Component } from '@angular/core';
 
 @Component({
@@ -8,8 +9,25 @@ import { Component } from '@angular/core';
   templateUrl: 'tabs.html',
 })
 export class TabsPage {
-  promoPage =  PromosPage;
-  mercadosPage = MercadosPage;
+  promoPage = PromosPage;
+  mercadosPage = HomePage;
   cartPage = CartPage;
+  perfilPage = MenuPerfilPage;
 
+  constructor(
+    public navCtrl: NavController,
+    public modalCtrl: ModalController,
+  ){
+
+  }
+}
+
+// Página de Perfil
+@Component({
+  selector: 'menu-perfil',
+  templateUrl: 'menu-perfil.html',
+})
+
+export class MenuPerfilPage {
+  constructor(){}
 }
